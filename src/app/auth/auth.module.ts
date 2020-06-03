f45579a077b95c093ba7227869b66d7d55f4e29b
Mohsen <kelarens@gmail.com>
2020-06-03 10:32:58 +0430

    signin and signup components added to Auth module

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import { SignupComponent } from './signup/signup.component';
+import { SigninComponent } from './signin/signin.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [SignupComponent, SigninComponent],
   imports: [
     CommonModule,
     AuthRoutingModule
